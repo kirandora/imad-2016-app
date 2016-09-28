031ff7edff9df50d38612821dc2dabbac7c94da5
Kiran Dora <kiran27swi@gmail.com>
2016-09-28 07:43:09 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value Has been entered';
+
+//move the image madi
+
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+    img.style.marginLeft = '100px';
+};
